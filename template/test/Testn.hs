1640ca00f50ac36cebb58fc33ee92ceedda734c4
liz <web@stillinbeta.com>
2017-12-02 13:41:12 -0500

    Fix typo in Test template

--- template/test/Testn.hs
+++ template/test/Testn.hs
@@ -5,7 +5,7 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  describe "day1" $ do
+  describe "dayn" $ do
     it "should pass part a" $ do
       1 `shouldBe` 1
     it "should pass part b" $ do
